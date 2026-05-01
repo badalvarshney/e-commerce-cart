2ee52285411c7b64f76e007c0bfef702e388e09c
BADAL_VARSHNEY <badalwps@gmail.com>
2026-05-01 17:08:06 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import RootLayout from './Pages/Root';
+// import HomePage from './Pages/HomePage/HomePage';
+// import ProductDetails from './Pages/ProductDetails/ProductDetails';
+// import Cart from './Pages/CartPage/Cart';
+// import Wishlist from './Pages/Wishlist/Wishlist';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <RootLayout />,
+//     children: [
+//       { index: true, element: <HomePage /> },
+//       { path: '/products/:id', element: <ProductDetails /> },
+//       { path: '/cart', element: <Cart /> },
+//       { path: '/wishlist', element: <Wishlist /> },
+//     ],
+//   },
+// ]);
+
+// function App() {
+//   return (
+//   <>
+//     <RouterProvider router={router} />;
+//   </>)
+
+// }
+
+// export default App;
+
+
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './Pages/Root';
 import HomePage from './Pages/HomePage/HomePage';
 import ProductDetails from './Pages/ProductDetails/ProductDetails';
 import Cart from './Pages/CartPage/Cart';
 import Wishlist from './Pages/Wishlist/Wishlist';
+
 
 const router = createBrowserRouter([
   {
